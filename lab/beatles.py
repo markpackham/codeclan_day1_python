e1112eb4b6e005bc3f4ae6f78ca9706deae8cb81
mark <markpackham1@gmail.com>
2020-08-03 16:21:48 +0100

    Update beatles test

--- lab/beatles.py
+++ lab/beatles.py
@@ -11,20 +11,23 @@ beatles = [
 # Use the `beatles` list above to answer the following questions:
 
 # 1. John Lennon also plays guitar. Access the `instrument` key in his dictionary and change its value:
-print(beatles[0])
-beatles[0]["instrument"] = "guitar"
-print(beatles[0])
+beatles[0]["instrument"] = "piano & guitar"
+print(beatles[0]["instrument"])
 
 # 2. Write a function which takes in the list of band members as a parameter,
 #    and returns a list of all the Beatles' names:
 # Expected result: ['John Lennon', 'Paul McCartney', 'George Harrison', 'Ringo Starr']
-def get_names(members):
-    names = []
-    for member in members:
-        names.append(member["name"])
-    return names
+# def get_names(members):
+#     names = []
+#     for member in members:
+#         names.append(member["name"])
+#     return names
 
-print(get_names(beatles))
+# print(get_names(beatles))
+
+def get_names_comprehension(members):
+    return [member["name"] for member in members]
+print(get_names_comprehension(beatles))
 
 # 3. Write a function which takes in the list of band members as a parameter,
 #    and returns a list of the members who are still alive
@@ -35,17 +38,22 @@ print(get_names(beatles))
 #    {'name': 'Ringo Starr', 'birth_year': 1940, 'death_year': None, 'instrument': 'drums'}
 # ]
 
-def get_alive(members):
-    alive = []
-    for member in members:
-        if member["death_year"] == None:
-            alive.append(member)
-    return alive
+# def get_alive(members):
+#     alive = []
+#     for member in members:
+#         if member["death_year"] == None:
+#             alive.append(member)
+#     return alive
 
-print(get_alive(beatles))
+# print(get_alive(beatles))
+
+
+def get_alive_comprehension(members):
+    return [member for member in members if member["death_year"] == None]
+print(get_names_comprehension(beatles))
 
 # 4. Combine the above two functions to return the names of all the members who are alive:
 # Expected result: ['Paul McCartney', 'Ringo Starr']
 
-alive_beatles = get_alive(beatles)
-print(get_names(alive_beatles))
+alive_beatles = get_alive_comprehension(beatles)
+print(get_names_comprehension(alive_beatles))
