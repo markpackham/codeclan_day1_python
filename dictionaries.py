00fee7099e157587039d947fb7754412c23c29e3
mark <markpackham1@gmail.com>
2020-08-03 11:36:57 +0100

    Dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,32 @@
+# This is a Hash or HashMap in other languages so key value pair
+# user = {"name":"Mark","money":10}
+# print(user)
+# print(user["money"])
+# user["email"]="mark@gmail.com"
+# print(user)
+# del(user["email"])
+# print(user)
+
+beatles = [
+  { "name": "John", "instrument": "guitar" },
+  { "name": "Paul", "instrument": "bass" },
+  { "name": "George", "instrument": "guitar" },
+  { "name": "Ringo", "instrument": "drums" }
+]
+
+# Get Paul
+print(beatles[1]["name"])
+
+user = {
+    "name":"Mark",
+    "age":19,
+    "students": ["Timmy", "Bobby"]
+}
+
+print(user)
+
+myself = [
+    {"name":"Mark", "age": 20, "students":[{"name":"Alice"},{"name":"Alice"}]}
+]
+
+print(myself)
